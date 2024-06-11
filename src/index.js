2cf208292370c18fc15500f90fdec8ed559386f0
Binh 🌱 <binhrobles@gmail.com>
2024-06-11 15:07:48 -0400

    first version of feedback and repair modes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,73 @@
-import 'dotenv/config';
-import openai from './models/openai.js';
+import "dotenv/config";
+import dirTree from "directory-tree";
+import fs from "fs";
+import inquirer from "inquirer";
+import openai from "./models/openai.js";
+
+const rip_tree = (dir) => {
+  let content = "";
+
+  dir.children.forEach((child) => {
+    if (!child.children) {
+      // then this is a file at this level of the directory
+      // rip it's contents and add to content
+      content += child.path + "\n";
+      content += "```\n";
+      content += fs.readFileSync(child.path);
+      content += "```\n\n";
+    } else {
+      content += rip_tree(child);
+    }
+  });
+
+  return content;
+};
 
 const main = async () => {
-  const response = await openai.getPrediction('oops');
-  console.log(response?.choices[0].message.content);
-}
+  const repo = process.argv[2];
+  const model = openai;
+
+  const options = await inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "mode",
+        message: "what would you like me to do?",
+        choices: [
+          { name: "roast me fam", value: "roast" },
+          { name: "make some edits", value: "repair" },
+        ],
+      },
+    ]);
+
+  const tree = dirTree(repo);
+  const content = rip_tree(tree);
+  fs.writeFileSync("./unified-project.md", content);
+
+  const costEstimate = model.estimateCost(content);
+  const { confirm_cost } = await inquirer.prompt({
+    name: "confirm_cost",
+    type: "confirm",
+    message: `Estimated generation cost: $${costEstimate}\n Okay to proceed?`,
+  });
+  if (!confirm_cost) {
+    console.log("fine then");
+    process.exit(0);
+  }
+
+  const response = await model.getPrediction(content, options);
+  const responseContent = response?.choices[0].message.content;
+
+  if (options.mode === 'repair') {
+    response.files.forEach(file => {
+      const { path, descriptionOfChanges, fileContent } = file;
+      fs.writeFileSync(path, JSON.parse(fileContent));
+      console.log(`Changed ${path} -- ${descriptionOfChanges}`);
+    });
+  }
+
+  fs.writeFileSync("./response.json", responseContent);
+  console.log(`this cost you $${model.getCost(response)}`);
+};
 
 main();
--- src/models/openai.js
+++ src/models/openai.js
@@ -1,19 +1,27 @@
-import OpenAI from 'openai';
+import OpenAI from "openai";
 
-export const getPrediction = async (content) => {
-  const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY })
-  console.log('making request...');
+export const getPrediction = async (content, options = {}) => {
+  const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+  const { mode } = options;
+  console.log(`mode: ${mode} -- reading through your code...`);
 
   try {
     const response = await openai.chat.completions.create({
-      model: 'gpt-4-turbo',
+      model: "gpt-4o",
+      response_format: { type: "json_object" },
       messages: [
         {
-          role: 'system',
-          content: 'You’re a staff software architect, highly capable of identifying common software and design anti-patterns. Given the following repo, identify if this repo is at risk of using any of the anti-patterns [or not following general software design best practices] [or language or framework-specific idiomatic best practices]. Don’t give general advice, keep it tailored for this codebase',
+          role: "system",
+          content: `
+You’re a staff software architect, highly capable of identifying common software and design anti-patterns. Given the following repo, identify if this repo is at risk of using any software architecture anti-patterns, not following general software design best practices, or not following language or framework-specific idiomatic best practices. Don’t give general advice, keep it tailored for this codebase.
+
+${mode === 'repair'
+              ? 'Implement your suggested changes. Return the changed files in JSON format with the shape: { files: [{ path: "", descriptionOfChanges: "", fileContent: "", }... ] }'
+              : 'Use this as an opportunity to mentor; go long with your descriptions of the issues, and speak to the best practice concepts. Inline all code references with filenames and line numbers. Return your response in JSON format with the shape: { issues: [{ issue: "", description: "", codeExample: "", suggestions: [""] }... ] }'
+            }`,
         },
         {
-          role: 'user',
+          role: "user",
           content,
         },
       ],
@@ -21,10 +29,26 @@ export const getPrediction = async (content) => {
 
     return response;
   } catch (error) {
-    throw error
+    throw error;
   }
-}
+};
+
+const getCost = (response) => {
+  const { completion_tokens, prompt_tokens } = response.usage;
+  return ((prompt_tokens / 1000) * 0.005 + (completion_tokens / 1000) * 0.015).toFixed(4); // gpt-4o pricing
+};
+
+const estimateCost = (content) => {
+  const inputTokens = content.length / 4;
+
+  const outputTokens = 1000; // assumed
+
+  const cost = (inputTokens / 1000) * 0.005 + (outputTokens / 1000) * 0.015; // gpt-4o pricing
+  return cost.toFixed(4);
+};
 
 export default {
   getPrediction,
-}
+  estimateCost,
+  getCost,
+};
